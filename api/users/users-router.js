1d2170e7f2fd439166271fb11147fd4520b4c028
unknown <xiongjoh@gmail.com>
2020-12-26 19:39:59 -0600

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,26 @@
 const router = require('express').Router()
 const Users = require('./users-model')
 
+router.get('/:username', async (req, res) => {
+    try {
+        
+    } catch(err) {
+        res.status(500).json({message:`Error with something along /username path`})
+    }
+})
+router.post('/register', async (req, res) => {
+    try {
 
+    } catch(err) {
+        res.status(500).json({message:`Error with something along /register path`})
+    }
+})
+router.post('/login', async (req, res) => {
+    try {
+
+    } catch(err) {
+        res.status(500).json({message:`Error with something along /login path`})
+    }
+})
 
 module.exports = router
